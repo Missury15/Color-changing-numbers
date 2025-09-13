96195a1c69a4de428bb3f7a4887b1718075597e3
Missury15 <tadeas.hajek.cz@gmail.com>
2025-09-13 12:58:29 +0200

    Create Main.cs for random number generation
    Initial implementation of a simple console application that generates a random number.

---
+++ Main.cs
@@ -0,0 +1,14 @@
+using System;
+{
+
+namespace Main
+{
+  public static void Main(string[] args)
+
+  Console.WriteLine("this script works")
+  Random randomNumbers = new Random();
+  int num01 = randomNumbers.Next(1,11);
+  Console.WriteLine(randomNumbers)
+    //dodělat doma
+    }
+}
